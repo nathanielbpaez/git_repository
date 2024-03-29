438d8fa31249b2654e0fa449c119eda8007e29de
Nathaniel Paez <152954759+nathanielbpaez@users.noreply.github.com>
2024-03-29 11:25:13 -0400

    Update a1.py
    final revis

--- a1.py
+++ a1.py
@@ -1,21 +1,16 @@
 '''
-auth: Nathaniel B. Paez
+auth @ Nathaniel B. Paez
 student_ID: 501228087
 date: 2024/03/19
 
-    The objective of this program is to be a calculator in preperation for MTH108 
-in the following year as I am an internal program transfer student. As well as,
-a support to students pursuing vector related mathematics such as "Calc 3" and
-other geometric maths.
+The objective of this program is to be a calculator in preperation for MTH108 Linear algebra
 
-The following program is equipped to swiftly solve both simple and complex concepts and are integrated
-with the math import to allow for percise calculations.
+The following program is equipped to swiftly solve both simple and complex concepts.
+The program will ask you; what you would like to do. If you choose a vector option,
+you will pre required to input 2 3d vectors.
 
-!!! NOTE !!!
-The with ... as ...:
-    ....
-    ....
-was learned from self research as I wanted to develop a watermark for dates and history (in case i need to revisit)
+Should you enter a zero vector or the same vector twice, the calculator will return a zero vector
+or a variable displaying it's equal to itself.
 '''
 
 #imports
@@ -25,7 +20,7 @@ from datetime import datetime as dt
 # UPON OPEN / MAIN FUNCTIONALITY
 
 def upon_open(): #runs to record dates
-    with open('vector_calculator_history.txt', 'a') as vector_history:
+    with open('output.txt', 'a') as vector_history:
         vector_history.write('\nDate edited: ' + str(dt.now()) + '\n')
 
 #determine what calculator the user wants to use
@@ -33,14 +28,15 @@ def main(): #everything is ran through vector checker to allow for optimal funct
     #options
     print ("\nWhat vector related question would you like to calculate?\n")
     print (" 0. Write additional notes") 
-    print (" 1. What is the angle between two 3D vectors?")
-    print (" 2. What is the area of the parallelogram from two 3D vectors?")
+    print (" 1. Do you want to know the angle between two 3D vectors?")
+    print (" 2. Do you want to know the area of the parallelogram from two 3D vectors?")
+    print (" 3. Do you want to know the cross product of two 3D vectors?")
     print ("-1. Clear history (NOTE: CANNOT UNDO)\n")
 
     #for future reference: insert new equations here
 
     checker = int(input("Please select a number corsponding to the previous list: "))
-    with open('vector_calculator_history.txt', 'a') as vector_history: #REFER TO LINE 18
+    with open('output.txt', 'a') as vector_history: #REFER TO LINE 18
         # 0. FREE WRITE
         if checker == 0:
             vector_history.write("\n" + str(input("NOTES: \n")) + "\n")
@@ -55,25 +51,33 @@ def main(): #everything is ran through vector checker to allow for optimal funct
             aOfv, a, b = vector_area()
             vector_history.write(f'\nThe area of the parallelogram in vectors {a} and {b} is {aOfv}\n')
 
+        # 3. CROSS PRODUCT
+        if checker == 3:
+            cross_prod, a, b = cross_prod_calc()
+            vector_history.write(f'\nThe cross product of {a} and {b} is {cross_prod}\n')
+
         # -1. CLEAR
     if checker == -1:
-        with open('vector_calculator_history.txt', 'w') as vector_history:
+        with open('output.txt', 'w') as vector_history:
             vector_history.write("")
     repeat()
     return
 
 #repeat function (allows for constant edits)
 def repeat(): #repeats to allow for more calculations in one run
+    switch = False
     x = str(input("\nWould you like to do anything else? (Y/N): "))
     if x == "Y":
-        main() #recycles
+        switch = True
+        if switch == True: #I needed boolean some how
+            main() #recycles
     elif x == "N":
         return #exits program
     else:
         print('Incorrect input please try again.') #to make sure no error.
         return repeat()
     return
-
+    
 # this function will be called, and will ask the user for their inputs
 def vector_bucket():
     vect = [[],[]]
@@ -81,6 +85,14 @@ def vector_bucket():
         vect[i].append(int(input(f"Enter X{i+1}: ")))
         vect[i].append(int(input(f"Enter Y{i+1}: ")))
         vect[i].append(int(input(f"Enter Z{i+1}: ")))
+
+    #Checks for zero vectors since 0/b or a/0 does not work
+    if vect[0] == [0, 0, 0] or vect[1] == [0, 0, 0]: 
+        print("Error: a zero vectors in regards to a vector equals zero")
+        return "Zero Vector"
+    elif vect[0] == vect[1]:
+        print("Error: there is no value between the same vector.")
+        return "Equal Vectors"
     return vect
 
 # EQUATIONS
@@ -90,6 +102,10 @@ def vector_angle():
     vect = vector_bucket()
     a = vect[0]
     b = vect[1]
+    if vect == "Zero Vector":
+        return [0, 0, 0], "A", "a zero vector" 
+    elif vect == "Equal Vectors":
+        return a, a, a
 
     # calculate a*b
     a_b = []
@@ -116,13 +132,30 @@ def vector_angle():
 #The equation for the area of the parallelogram from two vectors (dot prod of 2 vects)
 def vector_area():
     vect = vector_bucket() #call for vector information
+    if vect == "Zero Vector":
+        return [0, 0, 0], "A", "a zero vector" 
+    elif vect == "Equal Vectors":
+        return a, a, a
+    
     a = vect[0] # is the first set of coords
     b = vect[1] # is the second set of coords
+
     a_b_cross_prod = cross_product(a, b) #calls cross prod func
     area_of_vector = m.sqrt(sum(x**2 for x in a_b_cross_prod)) #calculates the magnitude of cross prod
     area_of_vector = round(area_of_vector, 4)# rounds the value
     return area_of_vector, a, b
 
+def cross_prod_calc():
+    vect = vector_bucket()
+    if vect == "Zero Vector":
+        return [0, 0, 0], "A", "a zero vector" 
+    elif vect == "Equal Vectors":
+        return a, a, a
+    
+    a = vect[0]
+    b = vect[1]
+    a_b_cross_prod = cross_product(a, b)
+    return a_b_cross_prod, a, b
 # SUB EQUATIONS
 
 # when called it either returns from prev inputed rad or casts to deg
@@ -145,6 +178,3 @@ def cross_product(a, b):
 if __name__ == "__main__":
     upon_open() #watermarks the date
     main() #main function call
-    #
-    # test commit
-    # test commit #6
